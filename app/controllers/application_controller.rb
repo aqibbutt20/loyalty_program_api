ab340e0db3999beb702dfa01f5fe627ff6e9c7bc
aqibbutt20 <aqibbutt20@hotmail.com>
2025-05-20 17:36:47 -0400

    add simple autherization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_client!
+
+  private
+
+  def authenticate_client!
+    api_key = request.headers['X-API-Key']
+    unless api_key == Rails.application.credentials.client_api_key
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
+  end
 end
